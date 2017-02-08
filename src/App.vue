a48dfb429c04a6a2e2e328534fc35a5e81bb58d4
aOrz <rememberwz@gmail.com>
2017-02-08 18:06:13 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Header-items></Header-items>
+    <router-view></router-view>
+    <FooterElm></FooterElm>
+  </div>
+</template>
+<script>
+import Header from './components/header';
+import Footer from './components/footer';
+
+export default {
+  name: 'app',
+  components: {
+    HeaderItems: Header,
+    FooterElm: Footer
+  }
+}
+</script>
+
+<style>
+</style>
