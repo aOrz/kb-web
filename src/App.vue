1464ded4c8823353eedf0193c7807dd9ae764080
shujie <rememberwz@gmail.com>
2017-02-08 20:06:03 +0800

    donate

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <Header-items></Header-items>
-    <router-view></router-view>
+    <div class="content">
+      <Header-items></Header-items>
+      <router-view></router-view>
+    </div>
     <FooterElm></FooterElm>
   </div>
 </template>
@@ -18,5 +20,19 @@ export default {
 }
 </script>
 
-<style>
+<style type="text/scss">
+  html, body, #app {
+    height: 100%;
+  }
+  .content {
+    min-height: 100%;
+    height: auto;
+  }
+  .content:after {
+      content: '';
+      display: block;
+      width: 100%;
+      height: 41px;
+      clear: both;
+    }
 </style>
