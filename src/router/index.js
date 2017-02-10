e1ce9dbd06546bc62c8e78b25e76f52f8f954041
shujie <rememberwz@gmail.com>
2017-02-10 19:14:18 +0800

    异步加载组件

--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,26 @@ import Router from 'vue-router';
 Vue.use(Router);
 
 import searchComponent from 'components/search';
-import donateComponent from 'components/donate';
-import courseTableComponent from 'components/course_table';
-import calendarComponent from 'components/calendar';
+// import donateComponent from 'components/donate';
+// import courseTableComponent from 'components/course_table';
+// import calendarComponent from 'components/calendar';
+const calendarComponent = resolve => {
+  require.ensure(['../components/calendar.vue'], () => {
+    resolve(require('../components/calendar.vue'))
+  })
+}
+
+const courseTableComponent = resolve => {
+  require.ensure(['../components/course_table.vue'], () => {
+    resolve(require('../components/course_table.vue'))
+  })
+}
+
+const donateComponent = resolve => {
+  require.ensure(['../components/donate.vue'], () => {
+    resolve(require('../components/donate.vue'))
+  })
+}
 
 export default new Router({
   routes: [
@@ -35,5 +52,8 @@ export default new Router({
       name: 'calendar',
       component: calendarComponent
     }
-  ]
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
 })
