151998379b416f14cb542316a1d356b718eb6d21
shujie <rememberwz@gmail.com>
2017-02-10 10:06:10 +0800

    课表页调整

--- src/components/course_table.vue
+++ src/components/course_table.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="content">
     <div class="row table-width">
-        <div class="col-xs-16 col-md-16 col-md-8">
         <table v-show="course" class="table table-responsive table-bordered table-condensed table-striped">
             <tbody>
               <tr>
@@ -20,7 +19,6 @@
               </tr>
             </tbody>
           </table>    
-        </div>
       </div>
   </div>
 </template>
@@ -63,7 +61,6 @@
 
         }).then(response => {
           // success callback
-          console.log(response);
           loading.hide();
           if (response.status != 200) {
             weui.alert('网络错误');
@@ -112,24 +109,15 @@
   }
   .table-width {
       width: 150%;
-  }
-  .row {
-      margin-right: -15px;
-      margin-left: -15px;
+      max-width: 800px;
   }
   .table-bordered>thead>tr>th, .table-bordered>tbody>tr>th, .table-bordered>tfoot>tr>th, .table-bordered>thead>tr>td, .table-bordered>tbody>tr>td, .table-bordered>tfoot>tr>td {
       border: 1px solid #ddd;
   }
-  .col-xs-1, .col-sm-1, .col-md-1, .col-lg-1, .col-xs-2, .col-sm-2, .col-md-2, .col-lg-2, .col-xs-3, .col-sm-3, .col-md-3, .col-lg-3, .col-xs-4, .col-sm-4, .col-md-4, .col-lg-4, .col-xs-5, .col-sm-5, .col-md-5, .col-lg-5, .col-xs-6, .col-sm-6, .col-md-6, .col-lg-6, .col-xs-7, .col-sm-7, .col-md-7, .col-lg-7, .col-xs-8, .col-sm-8, .col-md-8, .col-lg-8, .col-xs-9, .col-sm-9, .col-md-9, .col-lg-9, .col-xs-10, .col-sm-10, .col-md-10, .col-lg-10, .col-xs-11, .col-sm-11, .col-md-11, .col-lg-11, .col-xs-12, .col-sm-12, .col-md-12, .col-lg-12 {
-    position: relative;
-    min-height: 1px;
-    padding-right: 15px;
-    padding-left: 15px;
-  }
   .table {
       width: 100%;
       max-width: 100%;
-      margin-bottom: 20px;
+      margin-bottom: 5px;
       font-size:0.5em;
       table-layout:fixed;
   }
