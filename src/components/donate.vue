e36c1f66deec33d74feb6f6a39ace9a0cc673f58
4013465w <rememberwz@gmail.com>
2017-07-09 08:59:42 +0800

    增加接口

--- src/components/donate.vue
+++ src/components/donate.vue
@@ -5,10 +5,10 @@
             <section>
                 <!-- <h3>1.1 节标题</h3> -->
                 <p>迷你课表是一个完全公益的一个项目，目前由烟大的一个小团队维护，我们致力于让同学们更加方便快捷的查询课程表，摆脱不知道在哪上课的烦恼。</p>
-                <p>我们所有的团队成员都是无偿参与这个项目，项目至今已有两年多的时间，随着用户量的不断增加，我们的服务器成本也在增加，我们都是学生，没有太多的经济收入，您的捐助将会被用于我们的日常维护开销，以便更好的提供服务。</p>
+                <p>我们所有的团队成员都是无偿参与这个项目，项目至今已有两年多的时间，随着用户量的不断增加，我们的服务器成本也在增加，您的捐助将会被用于我们的日常维护开销，以便更好的提供服务。</p>
             </section>
             <section>
-                <h3 class="title">赠人玫瑰，手留余香，感谢您的慷慨解囊。</h3>
+                <h3 class="title">赠人玫瑰，手留余香，感谢您的慷慨解囊。捐赠请备注“迷你课表”</h3>
                 <h4>微信捐赠(长按识别或保存二维码)</h4>
                 <p class="img">
                     <img src="../assets/wzf.png">
@@ -30,50 +30,10 @@
         <section>
             <table class="donate">
                 <tbody>
-                    <tr>
-                        <td>未知</td>
-                        <td>5元</td>
-                        <td>微信转账</td>
-                    </tr>
-                    <tr>
-                        <td>未知</td>
-                        <td>10元</td>
-                        <td>微信转账</td>
-                    </tr>
-                    <tr>
-                        <td>未知</td>
-                        <td>10元</td>
-                        <td>微信转账</td>
-                    </tr>
-                    <tr>
-                        <td>未知</td>
-                        <td>10元</td>
-                        <td>微信转账</td>
-                    </tr>
-                    <tr>
-                        <td>未知</td>
-                        <td>3元</td>
-                        <td>微信转账</td>
-                    </tr>
-                    <tr>
-                        <td>未知</td>
-                        <td>10元</td>
-                        <td>微信转账</td>
-                    </tr>
-                    <tr>
-                        <td>耳朵3</td>
-                        <td>50元</td>
-                        <td>支付宝转账</td>
-                    </tr>
-                    <tr>
-                        <td>六点半</td>
-                        <td>10元</td>
-                        <td>支付宝转账</td>
-                    </tr>
-                    <tr>
-                        <td>纷舞年华</td>
-                        <td>10元</td>
-                        <td>微信转账</td>
+                    <tr v-for="i in donate">
+                        <td>{{i.user}}</td>
+                        <td>{{i.money}}元</td>
+                        <td>{{i.kind}}</td>
                     </tr>
                 </tbody>
             </table>
@@ -84,10 +44,31 @@
 
 export default {
     data() {
-            return {
-                threadKey: 'juanzeng'
-            }
+        return {
+            threadKey: 'juanzeng',
+            donate: []
         }
+    },
+    created () {
+       let that = this;
+      this.$http.get(`http://easy-mock.com/mock/595f7eac9adc231f357b9d78/minikb/inde/donate`, {
+          // use before callback
+          before(request) {
+
+              // abort previous request, if exists
+              if (this.previousRequest) {
+                  this.previousRequest.abort();
+              }
+
+              // set previous request on Vue instance
+              this.previousRequest = request;
+          }
+      }).then(response => {
+          if (response.status == 200) {
+              that.donate = response.data;
+          }
+      }); 
+    }
 }
 </script>
 <style type="text/scss" scoped>
